1c7c70e4ac8ee0e7d56765eb944967c8a1a46328
SonMooSans <god63820869@gmail.com>
2023-06-26 20:24:18 +0800

    Improve SEO

--- app.vue
+++ app.vue
@@ -3,3 +3,21 @@
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const image = new URL("/open-graph.png", useRequestURL());
+
+useSeoMeta({
+    titleTemplate: (titleChunk) => {
+        return titleChunk ? `${titleChunk} - Todo App` : "Todo App";
+    },
+    description: "A simple todo app",
+    ogTitle: "Todo App",
+    ogType: "website",
+    ogImage: image.toString(),
+    ogImageWidth: 900,
+    ogImageHeight: 473,
+    ogImageAlt: "Todo App",
+    twitterCard: "summary_large_image",
+});
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,26 @@
 <template>
     <div>
-        <p>This is a simple todo app built with Nuxt.js 3</p>
+        <h1 class="font-bold text-4xl">About</h1>
+        <p class="text-slate-400 mt-4">
+            This is a simple todo app built with Nuxt.js 3 and Tailwind CSS.
+        </p>
+        <div class="flex flex-row gap-4 mt-4">
+            <a
+                href="https://github.com/SonMooSans/todo-app-vue"
+                rel="noreferrer noopener"
+                class="px-6 py-1.5 font-medium text-sm text-slate-900 bg-cyan-300 rounded-lg transition-colors hover:bg-cyan-400"
+            >
+                Github
+            </a>
+        </div>
     </div>
 </template>
 
 <script setup>
+useSeoMeta({
+    title: "About",
+});
+
 definePageMeta({
     layout: "dashboard",
 });
